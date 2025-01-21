e92e0afd6489039eba6fe73306d0d0bc754a18f7
GoldenVadim <sujumai002@gmail.com>
2025-01-21 22:58:03 +0000

    Upgrade

--- tulpar.py
+++ tulpar.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from os import path,replace,chdir,remove,listdir,getuid,rename
+from os import path,replace,chdir,remove,listdir,getuid
 if getuid()!=0:
     print('You must run Tulpar as root user.')
     exit(1)
@@ -18,17 +18,6 @@ SUB.add_parser('remove').add_argument('PACKAGE')
 ARGPARSE.add_argument('-i',required=False)
 ARGS=ARGPARSE.parse_args()
 
-def move_contents(src_dir, dest_dir):
-    for item in listdir(src_dir):
-        src_path = path.join(src_dir, item)
-        dest_path = path.join(dest_dir, item)
-        
-        try:
-            shutil.move(src_path, dest_path)
-            print(f"Moved: {src_path} -> {dest_path}")
-        except Exception as e:
-            print(f"Failed to move {src_path} to {dest_path}: {e}")
-
 class ProceedInstall:
     def __init__(self,file_path:str):
         self.file_path=file_path
@@ -42,22 +31,17 @@ class ProceedInstall:
             if path.exists(path.join('/var/lib/tulpar/packages',self.metadata['name'])):
                 print(f' & "{self.metadata["name"]}" is already installed with version {self.metadata["version"]}')
                 self.cancel(1)
-            rename(self.file_path,self.metadata['name'])
-            self.file_path=path.abspath(self.metadata['name'])
-            replace(self.file_path,'/var/lib/tulpar/packages/')
+            replace(self.file_path,'/var/lib/tulpar/packages/'+self.metadata['name'])
             self.file_path='/var/lib/tulpar/packages/'+self.metadata['name']
             chdir(self.file_path)
-        elif path.isdir(self.file_path):
-            self.metadata=load(open(path.join(self.file_path,'metadata.json')))
-            chdir(self.file_path)
         print('--- Installing '+self.metadata['name'])
-        print(' * Checking checksums...')
         self.checksum()
         self.install()
     def cancel(self,exit_code:int):
         remove(self.file_path)
         exit(exit_code)
     def checksum(self):
+        print(' * Checking checksums...')
         if not path.exists('md5sums'):
             print(" - Checksum file is not found in package.")
             self.cancel(1)
@@ -68,7 +52,7 @@ class ProceedInstall:
         for line in checksums:
             parts = line.strip().split('  ')
             if len(parts) == 2:
-                expected_checksum, filename = parts
+                expected_checksum,filename=parts
                 file_path = path.join('data',filename)
                 if path.exists(file_path):
                     with open(file_path, 'rb') as file_content:
@@ -83,13 +67,15 @@ class ProceedInstall:
                 else:
                     print(f" - Missing file: {filename}")
                     self.cancel(1)
+    def __move_contents(self,src_dir,dest_dir):
+        for item in listdir(src_dir):
+            src_path = path.join(src_dir,item)
+            if path.isdir(src_path):shutil.copytree(src_path,dest_dir,True,dirs_exist_ok=True)
+            else:shutil.copy(src_path,dest_dir)
+            print(f"Moved: {src_path} -> {dest_dir}")
     def install(self):
-        print(listdir('data'))
-        move_contents('data','/')
+        self.__move_contents(path.abspath('data'),'/')
         print(f'-+- Successfully installed "{self.metadata["name"]}"')
 
 if ARGS.i is not None:
-    print(ARGS.i)
     ProceedInstall(ARGS.i)
-else:
-    print("No input file specified.")
