7fcd488483e9ee19320abc6240a91caa044f28a8
AnmiTaliDev <anmitali198@gmail.com>
2025-12-27 18:57:30 +0500

    Большой апдейт

--- libapg/include/apg/archive.h
+++
@@ -1,8 +0,0 @@
-// NurOS Ruzen42 2025 apg/archive.h
-// Last change: Dec 21
-#ifndef APG_ARCHIVE_H
-#define APG_ARCHIVE_H
-
-void unarchive_package(const struct package *pkg, const char* path);
-
-#endif
